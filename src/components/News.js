eb724eedd8fd2cdbce15e309675c84b47fecfe4a
VIPdemon25 <86335482+VIPdemon25@users.noreply.github.com>
2023-05-09 20:56:57 +0530

    Added Loading logic and props

--- src/components/News.js
+++ src/components/News.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
   constructor() {
@@ -12,11 +13,13 @@ export class News extends Component {
   }
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=389985488580449db9ff0ccb47a7d1d4&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsed_data = await data.json();
     this.setState({
       articles: parsed_data.articles,
       totalResults: parsed_data.totalResults,
+      loading: false,
     });
     // console.log(parsed_data);
   }
@@ -25,24 +28,27 @@ export class News extends Component {
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=389985488580449db9ff0ccb47a7d1d4&page=${
       this.state.page - 1
     }&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
     let data = await fetch(url);
     let parsed_data = await data.json();
     this.setState({
       page: this.state.page - 1,
       articles: parsed_data.articles,
+      loading: false,
     });
   };
   handleNextClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=389985488580449db9ff0ccb47a7d1d4&page=${
       this.state.page + 1
     }&pageSize=${this.props.pageSize}`;
-    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
-    } else {
+    this.setState({ loading: true });
+    if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / 20))) {
       let data = await fetch(url);
       let parsed_data = await data.json();
       this.setState(() => ({
         page: this.state.page + 1,
         articles: parsed_data.articles,
+        loading: false,
       }));
       console.log(parsed_data);
     }
@@ -50,22 +56,26 @@ export class News extends Component {
   render() {
     return (
       <div className="container my-2">
-        <h2>NewsMonkey Top Headlines</h2>
+        <h2 className="text-center">NewsMonkey Top Headlines</h2>
+        {this.state.loading && <Spinner />}
         <div className="row my-4">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={element.title ? element.title.slice(0, 45) : ""}
-                  description={
-                    element.description ? element.description.slice(0, 88) : ""
-                  }
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                />
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={
+                      element.description
+                        ? element.description.slice(0, 88)
+                        : ""
+                    }
+                    imageUrl={element.urlToImage}
+                    newsUrl={element.url}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
