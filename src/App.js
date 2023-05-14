2a1053b9c310a5e8fd55278988cc1db554fd1651
VIPdemon25 <86335482+VIPdemon25@users.noreply.github.com>
2023-05-14 21:05:47 +0530

    Initial Routing done for categories

--- src/App.js
+++ src/App.js
@@ -1,16 +1,106 @@
-import "./App.css";
-
 import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <News pageSize={18} country={"in"} category={"sports"} />
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <News
+                  key="general"
+                  pageSize={18}
+                  country={"in"}
+                  category={"general"}
+                />
+              }
+            />
+            <Route
+              path="/general"
+              element={
+                <News
+                  key="general"
+                  pageSize={18}
+                  country={"in"}
+                  category={"general"}
+                />
+              }
+            />
+            <Route
+              path="/business"
+              element={
+                <News
+                  key="business"
+                  pageSize={18}
+                  country={"in"}
+                  category={"business"}
+                />
+              }
+            />
+            <Route
+              path="/entertainment"
+              element={
+                <News
+                  key="entertainment"
+                  pageSize={18}
+                  country={"in"}
+                  category={"entertainment"}
+                />
+              }
+            />
+            <Route
+              path="/health"
+              element={
+                <News
+                  key="health"
+                  pageSize={18}
+                  country={"in"}
+                  category={"health"}
+                />
+              }
+            />
+            <Route
+              path="/science"
+              element={
+                <News
+                  key="science"
+                  pageSize={18}
+                  country={"in"}
+                  category={"science"}
+                />
+              }
+            />
+            <Route
+              path="/sports"
+              element={
+                <News
+                  key="sports"
+                  pageSize={18}
+                  country={"in"}
+                  category={"sports"}
+                />
+              }
+            />
+            <Route
+              path="/technology"
+              element={
+                <News
+                  key="technology"
+                  pageSize={18}
+                  country={"in"}
+                  category={"technology"}
+                />
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
     );
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,85 +1,66 @@
-// import PropTypes from "prop-types";
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
-export class Navbar extends Component {
-  static propTypes = {};
-
+export default class Navbar extends Component {
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/">
-              NewsMonkey
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Business
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Entertainment
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    General
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Health
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Science
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Sports
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/">
-                    Technology
-                  </a>
-                </li>
-              </ul>
-            </div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            NewsMonkey
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/general" exact="true">
+                  General
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/business">
+                  Business
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/entertainment">
+                  Entertainment
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/health">
+                  Health
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/science">
+                  Science
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/sports">
+                  Sports
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/technology">
+                  Technology
+                </Link>
+              </li>
+            </ul>
           </div>
-        </nav>
-      </div>
+        </div>
+      </nav>
     );
   }
 }
-
-export default Navbar;
